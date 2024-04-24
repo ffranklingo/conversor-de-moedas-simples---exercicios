705d6b31a969a0622c7262f0bfe629c71f1baebb
ffranklingo <franklingomes234@gmail.com>
2024-04-24 17:32:08 -0300

    Conversor de moedas simples

---
+++ 2.2 Problema 1.py
@@ -0,0 +1,5 @@
+cotacao = 3.45
+valor = input("Informe o valor da moeda estrangeira para compra: ")
+valor = float(valor)
+conversao = valor * cotacao
+print("o valor total em moeda local é: ", conversao)
